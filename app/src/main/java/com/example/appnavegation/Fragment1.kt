ad23a923f2538c88c14f986dedd9965f342db5bb
marcela27 <marcela.almeida17@etec.sp.gov.br>
2022-06-08 14:35:53 -0300

    Arquivos do AppNavegation

---
+++ app/src/main/java/com/example/appnavegation/Fragment1.kt
@@ -0,0 +1,25 @@
+package com.example.appnavegation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+
+class Fragment1 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_1, container, false)
+
+        view.setOnClickListener{ Navigation.findNavController(view).navigate(R.id.action_fragment1_to_fragment2) }
+
+        return view
+    }
+
+
+}
---
+++ app/src/main/java/com/example/appnavegation/Fragment2.kt
@@ -0,0 +1,24 @@
+package com.example.appnavegation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+
+class Fragment2 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_2, container, false)
+
+        view.setOnClickListener{ Navigation.findNavController(view).navigate(R.id.action_fragment2_to_fragment1) }
+
+        return view
+    }
+
+}
